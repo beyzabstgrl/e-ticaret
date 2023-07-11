f989ee9057e1b629cef7983610b85140d304453b
beyzanur basatogrul <beyza.basatogrul12@gmail.com>
2023-07-11 10:44:55 +0300

    Initial commit

---
+++ app/Http/Controllers/Frontend/PageHomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+
+class PageHomeController extends Controller
+{
+    public function index(){
+        return view('frontend.pages.index');
+    }
+}
